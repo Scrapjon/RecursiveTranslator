04e641cc9b86283d0c09046e6b1375d153423e7a
Oliver Moloney <127998765+Scrapjon@users.noreply.github.com>
2025-05-01 01:47:22 +1000

    Added support for large texts through text files

---
+++ batch_rectran.py
@@ -0,0 +1,16 @@
+from rectran import run_translate
+import asyncio
+
+# Allows for large texts with line breaks to be easily parsed and garbled.
+async def __main__():
+    with open("input.txt", "r") as f:
+        text_input = f.read()
+        print(text_input)
+        translated_output = await run_translate(text_input, 10)
+    with open("output.txt", "w") as f:
+        f.write(translated_output)
+        print(translated_output)
+
+if __name__ == "__main__":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # stop the runtime error grrrr
+    asyncio.run(__main__())
--- rectran.py
+++ rectran.py
@@ -5,20 +5,22 @@ import random, asyncio
 
 async def __main__():
     while True:
-        translator=Translator()
         text_input = input("What do you want translated?\nType 'exit' to quit.\n")
         if text_input == "exit":
             break
         elif text_input:
-            for i in range(10):
-                text_input = await translator.translate(text=text_input, dest=random.choice(list(LANGUAGES.values())))
-                text_input = text_input.text
-                print(f"Iteration {i}")
-            response = await translator.translate(text=text_input, dest="English")
-            print(response.text)
+            translated_output = await run_translate(text_input, 10)
+            print(translated_output)
         print("\n")
 
-
+async def run_translate(text_input: str, num: int) -> str:
+    translator=Translator()
+    for i in range(num):
+        text_input = await translator.translate(text=text_input, dest=random.choice(list(LANGUAGES.values())))
+        text_input = text_input.text
+        print(f"Iteration {i}")
+    response = await translator.translate(text=text_input, dest="English")
+    return response.text
     
 
 
